3f45aee752498149a06c97aa59dceffbea0232b3
A.Badalyan <timmirs@list.ru>
2021-06-21 18:31:30 +0300

    commit (use func map)

--- src/components/nav/links.js
+++ src/components/nav/links.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from './nav.module.css'
 
 function Links(prop) {
   return (
     <section>
-      <NavLink to={prop.url}> {prop.text} </NavLink>
+      <NavLink to={prop.url} activeClassName={styles.active}> {prop.text} </NavLink>
     </section>
   );
 }
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -2,15 +2,25 @@ import React from "react";
 import styles from './nav.module.css'
 import Links from "./links";
 
+
+let arr = [
+  { linkName: 'Главная страница', url: '/main' },
+  { linkName: 'Страница №1', url: '/link1' },
+  { linkName: 'Страница №2', url: '/link2' },
+  { linkName: 'Страница №3', url: '/link3' },
+  { linkName: 'Страница №4', url: '/link4' }
+]
+
+
 function Nav() {
+
+
+  let newArr = arr.map(el => <Links text={el.linkName} url={el.url} />)
+
   return (
-      <nav id={styles.nav}>
-        <Links text='Главная страница' url='/main'/>
-        <Links text='Страница №1' url='/link1'/>
-        <Links text='Страница №2' url='/link2'/>
-        <Links text='Страница №3' url='/link3'/>
-        <Links text='Страница №4' url='/link4'/>
-      </nav>
+    <nav id={styles.nav}>
+      {newArr}
+    </nav>
   );
 }
 
